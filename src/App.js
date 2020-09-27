e82ebb3ca2a57662e76c5cbce43b58e440edffae
Anthony San Miguel <asanmiguel1088@gmail.com>
2020-09-26 20:12:45 -0500

    completed part 1 of react space invaders tutorial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-
+import TitleScreen from './ReactComponents/TitleScreen';
+import InputManager from './InputManager';
 const width = 800;
 const height = window.innerHeight;
 const ratio = window.devicePixelRatio || 1;
@@ -9,6 +10,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      input: new InputManager(),
       screen: {
         width: width,
         height: height,
@@ -17,10 +19,18 @@ class App extends Component {
     }
   }
 
-  render() {
+  componentDidMount() {
+    this.state.input.bindKeys();
+  }
 
+  componentWillUnmount() {
+    this.state.input.unbindKeys();
+  }
+
+  render() {
     return (
         <div>
+          <TitleScreen />
           <canvas ref="canvas"
                   width={this.state.screen.width * this.state.screen.ratio}
                   height={this.state.screen.height * this.state.screen.ratio}/>
